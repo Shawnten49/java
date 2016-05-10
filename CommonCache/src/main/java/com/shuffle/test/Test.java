d1073bbac378233878f581757c0e9862088fc5d9
Shuffle <1633538293@qq.com>
2016-05-10 18:39:37 +0800

    CommonCache Test Class

---
+++ CommonCache/src/main/java/com/shuffle/test/Test.java
@@ -0,0 +1,14 @@
+package com.shuffle.test;
+
+/**
+ * Author:Shawn.Xu
+ * Date:2016/5/10
+ * Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("======");
+    }
+
+}
