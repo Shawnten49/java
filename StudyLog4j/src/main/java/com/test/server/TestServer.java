4167b116fe9ac0a094180c3fd165cc89c6f2bf7d
Shuffle <1633538293@qq.com>
2016-08-29 18:12:56 +0800

    StudyLog4j

---
+++ StudyLog4j/src/main/java/com/test/server/TestServer.java
@@ -0,0 +1,17 @@
+package com.test.server;
+
+
+/**
+ * Author:Shawn.Xu
+ * Date:2016/8/16
+ * Description:
+ */
+public class TestServer {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
